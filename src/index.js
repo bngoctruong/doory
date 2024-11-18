64dff4c993204890b98f681a59f2b502518a578a
bngoctruong <61890351+bngoctruong@users.noreply.github.com>
2024-11-17 22:49:15 -0800

    add axios api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+function searchCity(city){
+    let apiKey = "0bc5a8155a8t1ab5b31d93473cfdo8fc";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+console.log(apiURL);
+}
 let currentDay = document.querySelector("p .current-day");
 let currentTime = document.querySelector("p .current-time");
 let now = new Date();
@@ -6,6 +11,7 @@ currentDay.innerHTML = days[now.getDay()];
 let formattedHour = now.getHours();
 let formattedMin = now.getMinutes();
 currentTime.innerHTML = `${formattedHour}:${formattedMin}`;
+
 
 function inputCity(event) {
   event.preventDefault();
