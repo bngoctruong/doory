42d0d967d50dba1c4402c059e4bc45678e43d68e
bngoctruong <61890351+bngoctruong@users.noreply.github.com>
2024-11-18 00:44:56 -0800

    call input city's result and format date

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-function searchCity(city){
-    let apiKey = "0bc5a8155a8t1ab5b31d93473cfdo8fc";
-    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-console.log(apiURL);
+function getWeather(response) {
+ 
 }
+
+function displayResult(response) {
+  let cityElement = document.querySelector(".current-city");
+  cityElement.innerHTML = response.data.city;
+  let tempElement = document.querySelector("#current-temperature-value");
+ tempElement.innerHTML = Math.round(response.data.temperature.current);
+ let conditionElement = document.querySelector(".condition");
+ conditionElement.innerHTML = response.data.condition.description;
+let humidityElement = document.querySelector(".humidity");
+ humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+let windElement = document.querySelector(".wind-speed");
+ windElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
+
+function searchCity(event) {
+    event.preventDefault();
+    let inputCity = document.querySelector("#input-city");
+    let city = inputCity.value;
+    let apiKey = `0bc5a8155a8t1ab5b31d93473cfdo8fc`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(displayResult);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", searchCity);
+
 let currentDay = document.querySelector("p .current-day");
 let currentTime = document.querySelector("p .current-time");
 let now = new Date();
@@ -12,12 +36,3 @@ let formattedHour = now.getHours();
 let formattedMin = now.getMinutes();
 currentTime.innerHTML = `${formattedHour}:${formattedMin}`;
 
-
-function inputCity(event) {
-  event.preventDefault();
-  let myCity = document.querySelector("#input-city");
-  let currentCity = document.querySelector(".current-city");
-  currentCity.innerHTML = myCity.value;
-}
-let form = document.querySelector("form");
-form.addEventListener("submit", inputCity);
